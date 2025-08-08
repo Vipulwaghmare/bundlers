4ae98e3b3aaf704a2b5bc82923141c843f0d13ff
vipul <vipulwaghmare222@gmail.com>
2025-08-08 13:25:35 +0530

    added webpack

---
+++ packages/webpack/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const FirstComponent = () => {
+  return <h1>Hi Momsdf</h1>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<FirstComponent />);
---
+++ packages/webpack/webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
